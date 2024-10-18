214da563471dcb23318fc0cd877e3965dc50cf5c
Владислава Харитонова <vladislava-va@mail.ru>
2024-10-18 14:52:09 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+
+    try:
+        return a + b
+
+    except TypeError:
+        return str(a) + str(b)
+
+# Пример кода:
+print(add_everything_up(123.456, 'строка'))  # Вывод: '123.456строка'
+print(add_everything_up('яблоко', 4215))     # Вывод: 'яблоко4215'
+print(add_everything_up(123.456, 7))         # Вывод: 130.456
